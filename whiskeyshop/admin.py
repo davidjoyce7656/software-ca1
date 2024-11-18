99921135af00cb37615ac49c46448a98e8574dfd
David Joyce <davidjoyce1994@gmail.com>
2024-11-18 18:50:09 +0000

    sign in log out sign up added

--- whiskeyshop/admin.py
+++ whiskeyshop/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from .models import WhiskeyBrand
+# from django.contrib import admin
+# from .models import WhiskeyBrand
 
-# Register your models here.
-admin.site.register(WhiskeyBrand)
+# # Register your models here.
+# admin.site.register(WhiskeyBrand)
