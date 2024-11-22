16137b6bbe2cb05971cd0843a91f9e34a38bc757
Ben Rose <x00210478@mytudublin.ie>
2024-11-22 22:35:10 +0000

    whiskey info added to db pages created all whiskey page

--- whiskeycellar/urls.py
+++ whiskeycellar/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import JamesonPageView, MandMPageView
+from .views import AllWhiskeysPageView, JamesonPageView, MandMPageView, MidletonPageView, PowersPageView, RedBreastPageView, SpotPageView
 
 app_name = 'whiskeycellar'
 
 urlpatterns = [
+    path('all_whiskeys', AllWhiskeysPageView.as_view(), name='all_whiskeys'),
     path('jameson/', JamesonPageView.as_view(), name='jameson'),
     path('mandm/', MandMPageView.as_view(), name='mandm'),
+    path('midleton/', MidletonPageView.as_view(), name='midleton'),
+    path('powers/', PowersPageView.as_view(), name='powers'),
+    path("red_breast/", RedBreastPageView.as_view(), name='red_breast'),
+    path("spot/", SpotPageView.as_view(), name='spot')
 ]
--- whiskeycellar/views.py
+++ whiskeycellar/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import ListView
 from .models import Whiskey
 
+class AllWhiskeysPageView(ListView):
+    model = Whiskey
+    template_name = 'all_whiskeys.html'
+    context_object_name = 'all_whiskeys'
+
 class JamesonPageView(ListView):
     model = Whiskey
     template_name = 'jameson.html'
@@ -11,3 +16,22 @@ class MandMPageView(ListView):
     template_name = 'mandm.html'
     context_object_name = "mandm_whiskey"
 
+class MidletonPageView(ListView):
+    model = Whiskey
+    template_name = 'midleton.html'
+    context_object_name = "midleton_whiskey"
+    
+class PowersPageView(ListView):
+    model = Whiskey
+    template_name = 'powers.html'
+    context_object_name = "powers_whiskey"
+    
+class RedBreastPageView(ListView):
+    model = Whiskey
+    template_name = 'red_breast.html'
+    context_object_name = "red_breast_whiskey"
+
+class SpotPageView(ListView):
+    model = Whiskey
+    template_name = 'spot.html'
+    context_object_name = "spot_whiskey"
