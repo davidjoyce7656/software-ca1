dd64005e48bd8530468c47d649fa798051620a37
David <davidjoyce1994@gmail.com>
2024-11-28 10:45:16 +0000

    dbsql3 gitignore

--- whiskeycellar/models.py
+++ whiskeycellar/models.py
@@ -11,6 +11,13 @@ class Whiskey(models.Model):
     name = models.CharField(max_length=100)
     brand = models.ForeignKey('WhiskeyBrand', on_delete=models.CASCADE)
     description = models.TextField()
+    amount = models.TextField()
+    cask = models.TextField
+    distillery = models.TextField()
+    nose = models.TextField()
+    taste = models.TextField()
+    finish = models.TextField()
+    abv = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='whiskey_images/', null=True, blank=True)
     stock = models.IntegerField(null=True, blank=True)
